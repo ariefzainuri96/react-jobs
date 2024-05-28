15f62cd55841cc6f62e4e4b97d0d5744491f6a5c
Arief Zainuri <flaminglassoo1996@gmail.com>
2024-05-29 05:42:19 +0700

    update: setup shadcn/ui

--- src/layouts/main-layout.tsx
+++ src/layouts/main-layout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/navbar";
+import { Toaster } from "@/components/ui/toaster";
 
 const MainLayout = () => {
   return (
     <>
       <Navbar />
       <Outlet />
+      <Toaster />
     </>
   );
 };
