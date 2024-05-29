21cfb1c47d846b1f35a3dd5bca97fad15b34ab45
Arief Zainuri <flaminglassoo1996@gmail.com>
2024-05-30 00:39:01 +0700

    update: add skeleton in job list

--- src/components/job-listing/job-listing.tsx
+++ src/components/job-listing/job-listing.tsx
@@ -2,6 +2,7 @@ import useSWR from "swr";
 import { JobItem } from "../../model/job-item";
 import JobListItem from "./job-list-item";
 import { delay } from "../../utils/utils";
+import { Skeleton } from "../ui/skeleton";
 
 const JobListing = ({ showAll = false }: { showAll?: boolean }) => {
   const { data, error, isLoading } = useSWR("/jobs", async () => {
@@ -11,10 +12,6 @@ const JobListing = ({ showAll = false }: { showAll?: boolean }) => {
 
     return data;
   });
-
-  if (isLoading) {
-    return <p>Loading...</p>;
-  }
 
   if (error) {
     return <p>Error...</p>;
@@ -31,6 +28,14 @@ const JobListing = ({ showAll = false }: { showAll?: boolean }) => {
           </h2>
         )}
 
+        {isLoading && (
+          <div className="grid grid-cols-1 gap-6 md:grid-cols-3">
+            {[1, 1, 1].map((_) => {
+              return <JobListingSkeleton />;
+            })}
+          </div>
+        )}
+
         <div className="grid grid-cols-1 gap-6 md:grid-cols-3">
           {(showAll ? data ?? [] : (data ?? []).slice(0, 3)).map(
             (element, index) => {
@@ -44,3 +49,26 @@ const JobListing = ({ showAll = false }: { showAll?: boolean }) => {
 };
 
 export default JobListing;
+
+export const JobListingSkeleton = () => {
+  return (
+    <div className="flex flex-col rounded-xl bg-white shadow-md">
+      <div className="p-4">
+        <div className="mb-6">
+          <Skeleton className="my-2 h-3 w-[150px]" />
+          <Skeleton className="h-3 w-[150px] text-xl" />
+        </div>
+
+        <Skeleton className="mb-1 h-3 w-full" />
+        <Skeleton className="h-3 w-[230px]" />
+
+        <Skeleton className="mb-2 mt-4 h-3 w-[170px] text-indigo-500" />
+
+        <div className="mb-5 border border-gray-100"></div>
+
+        <Skeleton className="mb-4 h-3 w-[120px]" />
+        <Skeleton className="mb-4 h-[36px] w-full py-2" />
+      </div>
+    </div>
+  );
+};
