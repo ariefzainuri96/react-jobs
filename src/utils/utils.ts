2c84d95a2825490b0aef98e5494fb785a50918f1
Arief Zainuri <flaminglassoo1996@gmail.com>
2024-06-02 03:10:28 +0700

    remove unused code

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,12 +1,8 @@
 import { toast } from "@/components/ui/use-toast";
-import { mutate } from "swr";
 
 export function delay(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
-
-export const clearCache = () =>
-  mutate(() => true, undefined, { revalidate: false });
 
 type TSimpleToast = {
   title: string;
